2d820db3f79faab75372af5799681c5583c6a5fc
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-01-24 20:51:52 -0800

    map displays current location of browser

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,13 +4,35 @@ import { withGoogleMap, GoogleMap } from 'react-google-maps';
 class Map extends React.Component {
    constructor(props) {
       super(props);
-      this.state = { }
+      this.state = { userLocation : {lat:38 , lng:38}, loading:true}
    }
 
+   componentDidMount(props) {
+      navigator.geolocation.getCurrentPosition(
+         position => {
+            const { latitude, longitude } = position.coords;
+
+            this.setState({
+            userLocation: { lat: latitude, lng: longitude },
+            loading: false
+            });
+         },
+         () => {
+            this.setState({ loading: false });
+         }
+      );
+      } 
+
+
+
    render() {
+      const { userLocation, loading} = this.state;
+      if (loading) {
+         return null;
+      }
       const GoogleMapExample = withGoogleMap(props => (
          <GoogleMap
-           defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
+           defaultCenter = { userLocation }
            defaultZoom = { 13 }
          >
          </GoogleMap>
@@ -27,3 +49,4 @@ class Map extends React.Component {
 }
 
 export default Map;
+
