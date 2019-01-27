e22221d8800571963f475a1cfb2952ada1e5a01a
Sophia Jay Tiutan <stiutan@Sophias-MacBook-Pro.local>
2019-01-27 00:04:06 -0800

    header remove comment

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-
 const Header = () => {
    return(
       <header>
@@ -25,27 +24,6 @@ const Header = () => {
                </Nav>
             </Navbar.Collapse>
          </Navbar>
-         {/* <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-               <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-               <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                  <li class="active">
-                     <a class="scroll nav-link">
-                        <Link to='/'>Home</Link>
-                     </a>
-                  </li>
-                  <li class="">
-                     <a class="scroll nav-link">
-                        <Link to='/map'>Map</Link>
-                     </a>               
-                  </li>
-
-               </ul> 
-            </div>
-         </nav> */}
       </header>
    )
 }
