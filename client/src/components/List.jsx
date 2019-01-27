6b8d1cf72bf9b07031da55bae8fc73a4caaad83e
Sophia Jay Tiutan <stiutan@Sophias-MacBook-Pro.local>
2019-01-27 00:21:41 -0800

    add list component

---
+++ client/src/components/List.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class List extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = { };
+   }
+
+   render() {
+      return(
+         <div>
+            Insert ListItems in here.
+         </div>
+      );
+   }
+}
+
+export default List;
--- client/src/components/Vendors.jsx
+++ client/src/components/Vendors.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/Vendors.css';
 
+import List from './List.jsx';
 import Map from './Map.jsx';
 
 class Vendors extends React.Component {
@@ -13,13 +14,13 @@ class Vendors extends React.Component {
       return(
          <div className={ styles.outerContainer }>
             <div className={ styles.searchBar }>
-                Search
+               Search Bar Here
             </div>
             <div className={ styles.listColumn }>
-                List
+               <List />
             </div>
             <div className={ styles.mapColumn }>
-                <Map />
+               <Map />
             </div>
          </div>
       );
