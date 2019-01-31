9187e128e5e0c668ce16e1ac2c2e00105ca612e0
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-01-31 15:49:44 -0800

    changes made to Registration.jsx

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -5,35 +5,70 @@ class Registration extends React.Component {
    constructor(props) {
       super(props);
       this.state = { 
-         name: "Sonali",
-         phone: "",
-         address: "",
-         city: "",
-         state: "",
-         zip: "",
-         location: null,
+         name: '',
+         phone: '',
+         address: '',
+         city: '',
+         state: '',
+         zip: '',
+         coordinates: null,
          keywords: []
-      };
+      }
+      this.updateName = this.updateName.bind(this);
+      this.updatePhone = this.updatePhone.bind(this);
+      this.updateAddress = this.updateAddress.bind(this);
+      this.updateCity = this.updateCity.bind(this);
+      this.updateState = this.updateState.bind(this);
+      this.updateZip = this.updateZip.bind(this);
+      this.updateKeywords = this.updateKeywords.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
    }
 
    handleSubmit(event) {
       //we don't want the form to submit, so we prevent the default behavior
       event.preventDefault();
       console.log("submitting form");
-      console.log(this.state.name);
 
-      var registrationForm = [
-         name = this.state.name,
-         phone = this.state.phone,
-         address = this.state.address + " " + this.state.city + ", " + this.state.state + " " + this.state.zip,
-         location = null,
-         keywords = this.state.keywords.split(", ")
-      ];
-        //make a call to the server to create the vendor
-        fetch('/server/data/createVendor', {
-         method: 'POST',
-         body: registrationForm,
+      let completeAddress = this.state.address + " " +
+      this.state.city + ", " + this.state.state + " " + this.state.zip;
+
+      var vendor = {
+         "name": this.state.name,
+         "phone": this.state.phone,
+         "address": completeAddress,
+         "coordinates": this.state.coordinates,
+         "keywords": this.state.keywords
+       };
+       
+      console.log(vendor); 
+
+      //make a call to the server to create the vendor
+      fetch('https://github.com/manipushpak/cravings/blob/devika/server/data/datamanager.ts', {
+      method: 'POST',
+      body: vendor,
        });
+   }
+
+   updateName(e) {
+      this.setState({ name: e.target.value });
+   }
+   updatePhone(e) {
+      this.setState({ phone: e.target.value });
+   }
+   updateAddress(e) {
+      this.setState({ address: e.target.value });
+   }
+   updateCity(e) {
+      this.setState({ city: e.target.value });
+   }
+   updateState(e) {
+      this.setState({ state: e.target.value });
+   }
+   updateZip(e) {
+      this.setState({ zip: e.target.value });
+   }
+   updateKeywords(e) {
+      this.setState({ keywords: e.target.value });
    }
 
    render() {
@@ -46,7 +81,7 @@ class Registration extends React.Component {
 				   </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="text" value={this.state.name} className={styles.input} id="name" onChange={this.handleChange.bind(this)}  placeholder="Enter name of food stand" />
+                  <input type="text" value={this.state.name} className={styles.input} id="name" onChange={e => this.updateName(e)}  placeholder="Enter name of food stall" />
 				   </div>
 
                <div className = {styles.formrow}>
@@ -54,7 +89,7 @@ class Registration extends React.Component {
                </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="tel" value={this.state.inputValue} className={styles.input} id="phone" onChange={this.handleChange.bind(this)} placeholder="Enter phone number (xxx-xxx-xxxx)" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  <input type="tel" value={this.state.phone} className={styles.input} id="phone" onChange={e => this.updatePhone(e)} placeholder="Enter phone number (xxx-xxx-xxxx)" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                      required></input>
 				   </div>
 
@@ -63,7 +98,7 @@ class Registration extends React.Component {
                </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="text" value={this.state.inputValue} className={styles.input} id="adr" name="address" onChange={this.handleChange.bind(this)}  placeholder="Enter address"></input>
+                  <input type="text" value={this.state.address} className={styles.input} id="adr" name="address" onChange={e => this.updateAddress(e)}  placeholder="Enter address"></input>
                </div>
                
 
@@ -72,7 +107,7 @@ class Registration extends React.Component {
                </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="text" className={styles.input} id="city" name="city" onChange={this.handleChange.bind(this)} placeholder="Enter city"></input>
+                  <input type="text" value={this.state.city} className={styles.input} id="city" name="city" onChange={e => this.updateCity(e)} placeholder="Enter city"></input>
                </div>
 
                <div className = {styles.formrow}>
@@ -80,7 +115,7 @@ class Registration extends React.Component {
                </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="text" value={this.state.inputValue} className={styles.input} id="state" name="state" onChange={this.handleChange.bind(this)} placeholder="Enter state"/>
+                  <input type="text" value={this.state.state} className={styles.input} id="state" name="state" onChange={e => this.updateState(e)} placeholder="Enter state"/>
                </div>
 
                <div className = {styles.formrow}>
@@ -88,14 +123,14 @@ class Registration extends React.Component {
                </div>
 
                <div className = {styles.formrowtwo}>
-                  <input type="text" value={this.state.inputValue} className={styles.input} id="zip" name="zip" onChange={this.handleChange.bind(this)}  placeholder="Enter zipcode"/>
+                  <input type="text" value={this.state.zip} className={styles.input} id="zip" name="zip" onChange={e => this.updateZip(e)}  placeholder="Enter zipcode"/>
                </div>
 
                <div className = {styles.formrow}>
                   <label>Keywords:</label>
                </div>
                <div className = {styles.formrowtwo}>
-                  <input type="text" value={this.state.inputValue} className={styles.input} id="keywords" name="keywords" onChange={this.handleChange.bind(this)} placeholder="Please enter the key search terms for your food stall"/>
+                  <input type="text" value={this.state.keywords} className={styles.input} id="keywords" name="keywords" onChange={e => this.updateKeywords(e)} placeholder="Enter search terms for your food stall"/>
                </div>
 
                <div className = {styles.formrowbutton}>
@@ -106,10 +141,6 @@ class Registration extends React.Component {
          </div>
       );
    }
-   handleChange(e) {
-      this.setState({ name: e.target.value });
-   }
 }
-
 
 export default Registration;
---
+++ client/src/components/Vendor.jsx
@@ -0,0 +1,9 @@
+export class Vendor {
+   constructor(name, phone, address, coordinates, keywords) {
+     this.name = name;
+     this.phone = phone;
+     this.address = address;
+     this.coordinates = coordinates;
+     this.keywords = keywords;
+   }
+ }
