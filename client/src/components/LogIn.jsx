1045614772248040fa6880559e7e228b2daf1eb1
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-02-09 20:59:40 -0800

    added forgot password

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -70,29 +70,34 @@ class LogIn extends React.Component {
                <h1>Log In</h1>
                <br />
                <Form noValidate validated={this.state.validated} onSubmit={e => this.handleSubmit(e)}>
-                  <Form.Row>
-                     <Form.Group as={Col} controlId="email" xs={12} md={12}>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control placeholder="Enter email" onChange={e => this.updateEmail(e)} required />
-                        <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
-                     </Form.Group>
-                  </Form.Row>
-                  <Form.Row>
-                     <Form.Group as={Col} controlId="password" xs={12} md={12}>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control placeholder="********" type="password" onChange={e => this.updatePassword(e)} required />
-                        <Form.Control.Feedback type="invalid">Please enter correct password.</Form.Control.Feedback>
-                     </Form.Group>
-                  </Form.Row>
-                  <Form.Row>
-                     <Button className={ styles.button } variant="primary" type="submit">
-                        Submit
-                     </Button>
-                     <Button className={ styles.button } variant="secondary" type="reset" onClick={ this.handleClearForm }>
-                        Reset
-                     </Button>
-                  </Form.Row>
-               </Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="email" xs={12} md={12}>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control placeholder="Enter email" onChange={e => this.updateEmail(e)} required />
+                            <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="password" xs={12} md={12}>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control placeholder="********" type="password" onChange={e => this.updatePassword(e)} required />
+                            <Form.Control.Feedback type="invalid">Please enter correct password.</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Button className={ styles.button } variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Button className={ styles.button } variant="secondary" type="reset" onClick={ this.handleClearForm }>
+                            Reset
+                        </Button>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="forgotpasword" xs={12} md={12}>
+                            <a class="small" href="#">Forgot password?</a>
+                        </Form.Group>
+                    </Form.Row>   
+                </Form>
             </div>
          </div>
       );
