692974d7a61adeb6280e4b63ae0f6311e96f72f6
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-02-09 22:03:12 -0800

    smol changes

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -60,44 +60,44 @@ class LogIn extends React.Component {
    }
 
    render() {
-      return(
-         <div className={styles.outercontainer}>
-            <div className={styles.column}>
-               <h1>Log In</h1>
-               <br />
-               <Form noValidate validated={this.state.validated} onSubmit={e => this.handleSubmit(e)}>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="email" xs={12} md={12}>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control placeholder="Enter email" onChange={e => this.updateEmail(e)} required />
-                            <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="password" xs={12} md={12}>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control placeholder="********" type="password" onChange={e => this.updatePassword(e)} required />
-                            <Form.Control.Feedback type="invalid">Please enter correct password.</Form.Control.Feedback>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Button className={ styles.button } variant="primary" type="submit">
-                            Submit
-                        </Button>
-                        <Button className={ styles.button } variant="secondary" type="reset" onClick={ this.handleClearForm }>
-                            Reset
-                        </Button>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} xs={12} md={12}>
-                            <a className="small" href="#">Forgot password?</a>
-                        </Form.Group>
-                    </Form.Row>   
-                </Form>
+        return(
+            <div className={styles.outercontainer}>
+                <div className={styles.column}>
+                <h1>Log In</h1>
+                <br />
+                <Form noValidate validated={this.state.validated} onSubmit={e => this.handleSubmit(e)}>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="email" xs={12} md={12}>
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control placeholder="Enter email" onChange={e => this.updateEmail(e)} required />
+                                <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="password" xs={12} md={12}>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control placeholder="********" type="password" onChange={e => this.updatePassword(e)} required />
+                                <Form.Control.Feedback type="invalid">Please enter correct password.</Form.Control.Feedback>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Button className={ styles.button } variant="primary" type="submit">
+                                Submit
+                            </Button>
+                            <Button className={ styles.button } variant="secondary" type="reset" onClick={ this.handleClearForm }>
+                                Reset
+                            </Button>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} xs={12} md={12}>
+                                <a className="small" href="#">Forgot password?</a>
+                            </Form.Group>
+                        </Form.Row>   
+                    </Form>
+                </div>
             </div>
-         </div>
-      );
-   }
+        );
+    }
 }
 
 export default LogIn;
