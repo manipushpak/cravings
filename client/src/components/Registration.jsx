a23c7433b57e77f9ecfc4601e0eff09f41c7fcc0
Sophia Jay Tiutan <stiutan@Sophias-MacBook-Pro.local>
2019-02-11 17:25:29 -0800

    location finally works praise the lorddd

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form';
 import StateOptions from './RegistrationElements/StateOptions.jsx';
 import TimeOptions from './RegistrationElements/TimeOptions.jsx';
+// import Geosuggest from 'react-bootstrap-geosuggest/';
 
 class Registration extends React.Component {
    constructor(props) {
@@ -13,8 +14,8 @@ class Registration extends React.Component {
       this.state = { 
          name: '',
          phone: '',
+         location: '',
          address: '',
-         address2: '',
          city: '',
          state: '',
          zip: '',
@@ -27,8 +28,8 @@ class Registration extends React.Component {
       }
       this.updateName = this.updateName.bind(this);
       this.updatePhone = this.updatePhone.bind(this);
+      this.updateLocation = this.updateLocation.bind(this);
       this.updateAddress = this.updateAddress.bind(this);
-      this.updateAddress2 = this.updateAddress2.bind(this);
       this.updateCity = this.updateCity.bind(this);
       this.updateState = this.updateState.bind(this);
       this.updateZip = this.updateZip.bind(this);
@@ -38,6 +39,7 @@ class Registration extends React.Component {
       this.updateKeywords = this.updateKeywords.bind(this);
       this.handleClearForm = this.handleClearForm.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.activatePlacesSearch = this.activatePlacesSearch.bind(this);
    }
 
    handleClearForm() {
@@ -45,8 +47,8 @@ class Registration extends React.Component {
       this.setState({
          name: '',
          phone: '',
+         location: '',
          address: '',
-         address2: '',
          city: '',
          state: '',
          zip: '',
@@ -66,8 +68,8 @@ class Registration extends React.Component {
          event.stopPropagation;
          this.setState({ validated: true });
       } else {
-         let completeAddress = this.state.address + (this.state.address2 === '' ? "" : " " + this.state.address2 )
-            + ", " + this.state.city + ", " + this.state.state + " " + this.state.zip;
+         let completeAddress = this.state.address + ", " + this.state.city
+         + ", " + this.state.state + " " + this.state.zip;
    
          fetch('/vendor/create',{
             method: 'POST',
@@ -95,11 +97,12 @@ class Registration extends React.Component {
    updatePhone(e) {
       this.setState({ phone: e.target.value });
    }
+   updateLocation(location) {
+      this.setState({ location: location });
+      console.log("hello, " + location);
+   }
    updateAddress(e) {
       this.setState({ address: e.target.value });
-   }
-   updateAddress2(e) {
-      this.setState({ address2: e.target.value });
    }
    updateCity(e) {
       this.setState({ city: e.target.value });
@@ -110,7 +113,8 @@ class Registration extends React.Component {
    updateZip(e) {
       this.setState({ zip: e.target.value });
    }
-   updateWeek(e) {
+   updateWeek(e, index) {
+      console.log(index);
       var newWeek = this.state.week.slice();
       if (e.target.checked) {
          newWeek.push(e.target.value);
@@ -129,7 +133,22 @@ class Registration extends React.Component {
       this.setState({ keywords: e.target.value.split(", ") });
    }
 
+   activatePlacesSearch() {
+      var self = this;
+      var autocomplete = new google.maps.places.Autocomplete(document.getElementById('vendorRegistrationLocation'));
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+         var place = autocomplete.getPlace();
+         getAddressAndCoordinates(place.formatted_address, place.geometry.location.lat(), place.geometry.location.lng());
+      });
+
+      function getAddressAndCoordinates(address, lat, lng) {
+         self.updateLocation(address);
+      }
+  }
+
    render() {
+      google.maps.event.addDomListener(window, 'load', this.activatePlacesSearch);
+
       return(
          <div className={styles.outercontainer}>
             <h1>Vendor Registration</h1>
@@ -149,15 +168,16 @@ class Registration extends React.Component {
                   </Form.Group>
                </Form.Row>
 
-               <Form.Group controlId="address">
-                  <Form.Label>Address</Form.Label>
-                  <Form.Control placeholder="1234 Main St" onChange={e => this.updateAddress(e)} required />
+               <Form.Group controlId="location">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control id="vendorRegistrationLocation" />
                   <Form.Control.Feedback type="invalid">Please enter your street address.</Form.Control.Feedback>
                </Form.Group>
 
-               <Form.Group controlId="address2">
-                  <Form.Label>Address 2 (optional)</Form.Label>
-                  <Form.Control placeholder="Apartment, studio, or floor" onChange={e => this.updateAddress2(e)} />
+               <Form.Group controlId="address">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control placeholder="1234 Main St." onChange={e => this.updateAddress(e)} required />
+                  <Form.Control.Feedback type="invalid">Please enter your street address.</Form.Control.Feedback>
                </Form.Group>
 
                <Form.Row>
@@ -189,13 +209,13 @@ class Registration extends React.Component {
                   <Form.Group as={Col} controlId="openingDaysAndTimes" xs={12} md={6}>
                      <Form.Label>Opening Days (optional)</Form.Label>
                      <Form.Group>
-                        <Form.Check inline label="M" value="Monday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="T" value="Tuesday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="W" value="Wednesday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="T" value="Thursday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="F" value="Friday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="S" value="Saturday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
-                        <Form.Check inline label="S" value="Sunday" type="checkbox" onChange={(e) => this.updateWeek(e)} />
+                        <Form.Check inline label="M" value="Monday" type="checkbox" onChange={(e) => this.updateWeek(e, 0)} />
+                        <Form.Check inline label="T" value="Tuesday" type="checkbox" onChange={(e) => this.updateWeek(e, 1)} />
+                        <Form.Check inline label="W" value="Wednesday" type="checkbox" onChange={(e) => this.updateWeek(e, 2)} />
+                        <Form.Check inline label="T" value="Thursday" type="checkbox" onChange={(e) => this.updateWeek(e, 3)} />
+                        <Form.Check inline label="F" value="Friday" type="checkbox" onChange={(e) => this.updateWeek(e, 4)} />
+                        <Form.Check inline label="S" value="Saturday" type="checkbox" onChange={(e) => this.updateWeek(e, 5)} />
+                        <Form.Check inline label="S" value="Sunday" type="checkbox" onChange={(e) => this.updateWeek(e, 6)} />
                      </Form.Group>
                      <Form.Label>Opening Hours (optional)</Form.Label>
                      <Form.Row>
@@ -218,6 +238,7 @@ class Registration extends React.Component {
                   </Button>
                </Form.Row>
             </Form>
+
          </div>
       );
    }
