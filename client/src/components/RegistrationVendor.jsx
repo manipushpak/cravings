a5ff83a16b79b1b267d5a89a3d12248ad8a9f8fe
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-02-13 19:49:52 -0800

    remove vendor hyperlink added

--- client/src/components/RegistrationVendor.jsx
+++ client/src/components/RegistrationVendor.jsx
@@ -12,16 +12,20 @@ class RegistrationVendor extends React.Component {
     render() {
         return (
             <Form.Row>
-                <Form.Group as={Col} controlId="firstName" xs={12} md={6}>
+                <Form.Group as={Col} controlId="firstName" xs={12} md={5}>
                     <Form.Label>First Name</Form.Label>
                     <Form.Control placeholder="Enter first name" required />
                     <Form.Control.Feedback type="invalid">Please enter your first name.</Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="lastName" xs={12} md={6}>
+                <Form.Group as={Col} controlId="lastName" xs={12} md={5}>
                     <Form.Label>Last Name</Form.Label>
                     <Form.Control placeholder="Enter last name" required />
                     <Form.Control.Feedback type="invalid">Please enter your last number.</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} controlId="remove" xs={12} md={1}>
+                    <br></br>
+                    <p><a href="void:()">Remove</a></p>
                 </Form.Group>
             </Form.Row>
         );
