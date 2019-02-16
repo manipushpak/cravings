de5d792378d3dcc5a388796a08603d9c0b14b028
Sophia Jay Tiutan <stiutan@Sophias-MacBook-Pro.local>
2019-02-16 02:42:13 -0800

    adding portal

--- client/src/components/Registration/ButtonSet.jsx
+++ client/src/components/Registration/ButtonSet.jsx
@@ -8,7 +8,7 @@ const ButtonSet = props => {
     if (props.status.view) {
         return (
             <div className={ styles.editButtonDiv }>
-                <Button variant="light" onClick={props.onClickEdit}><i class="far fa-edit"></i></Button>
+                <Button variant="light" onClick={props.onClickEdit}> <i class="far fa-edit" /></Button>
             </div>
         );
     } else if (props.status.edit) {
