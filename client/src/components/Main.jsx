fa3b6ccbaeb357db9a024b245f2c884ce26debe3
Sophia Jay Tiutan <stiutan@Sophias-MacBook-Pro.local>
2019-02-16 22:28:40 -0800

    added vendor portal links

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,9 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home.jsx';
 import About from './About/About.jsx';
+import Registration from './Registration/Registration.jsx';
+import UserLogInSignUp from './UserLogInSignUp/UserLogInSignUp.jsx';
 import Vendors from './Vendors/Vendors.jsx';
 import VendorPortal from './Registration/VendorPortal.jsx';
-import UserLogInSignUp from './UserLogInSignUp/UserLogInSignUp.jsx';
+
 
 const Main = () => {
    return(
@@ -15,7 +17,9 @@ const Main = () => {
             <Route exact path='/' component={Home}/>
             <Route path='/about' component={About}/>
             <Route path='/vendors' component={Vendors}/>
-            <Route path='/vendorportal' component={VendorPortal}/>
+            <Route exact path='/vendorportal' component={VendorPortal}/>
+            <Route path={'/vendorportal/register'} render={() => <Registration isEdit={false} isView={false}/>} />
+            <Route path={'/vendorportal/account'} render={() => <Registration isEdit={false} isView={true}/>} />
             <Route path='/userloginsignup' component={UserLogInSignUp}/>
          </Switch>
       </main>
--- client/src/components/Registration/VendorPortal.jsx
+++ client/src/components/Registration/VendorPortal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { Link, Route } from 'react-router-dom';
-import Registration from './Registration.jsx';
+import { Link } from 'react-router-dom';
 
 const VendorPortal = ({ match }) => {
     return(
@@ -9,9 +8,6 @@ const VendorPortal = ({ match }) => {
             <Link to={match.url + "/register"}>Click me to register!</Link>
             <br />
             <Link to={match.url + "/account"}>Click me to view your account!</Link>
-            <hr />
-            <Route path={match.url + "/register"} render={() => <Registration isEdit={false} isView={false}/>} />
-            <Route path={match.url + "/account"} render={() => <Registration isEdit={false} isView={true}/>} />
         </div>
     );
 }
