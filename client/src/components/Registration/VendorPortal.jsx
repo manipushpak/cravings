527c32679944a49dad68d3b9ee908cdf01349001
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-02-19 14:55:41 -0800

    vendor portal changes

--- client/src/components/Registration/VendorPortal.jsx
+++ client/src/components/Registration/VendorPortal.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import styles from '../../styles/Registration/VendorPortal.css';
 
-import { Link } from 'react-router-dom';
+import VendorLogIn from './VendorLogIn.jsx';
+import VendorSignUp from './VendorSignUp.jsx';
 
-const VendorPortal = ({ match }) => {
-    return(
-        <div>
-            <Link to={match.url + "/register"}>Click me to register!</Link>
-            <br />
-            <Link to={match.url + "/account"}>Click me to view your account!</Link>
-        </div>
-    );
+class VendorPortal extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = { 
+         logInHidden: false,
+         signUpHidden: true
+      }
+      this.toggleLogInSignUp = this.toggleLogInSignUp.bind(this);
+   }
+
+   toggleLogInSignUp () {
+      this.setState({
+        logInHidden: !this.state.logInHidden,
+        signUpHidden: !this.state.signUpHidden
+      });
+   }
+
+   render() {
+      return(
+         <div className={ styles.outerContainer }>
+            <div>
+               {!this.state.logInHidden && <VendorLogIn toggleLogInSignUp ={this.toggleLogInSignUp}
+                signUpHidden={this.props.signUpHidden}/>}
+            </div>
+            <div>
+               {!this.state.signUpHidden && <VendorSignUp toggleLogInSignUp ={this.toggleLogInSignUp}
+                signUpHidden={this.props.signUpHidden} />}
+            </div>
+         </div>
+      );
+   }
 }
 
 export default VendorPortal;
