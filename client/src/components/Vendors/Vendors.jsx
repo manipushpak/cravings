49e1f7cc8ef45984cedb38935181a1f78bfc7903
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-02-19 18:41:31 -0800

    search queries working

--- client/src/components/Vendors/Vendors.jsx
+++ client/src/components/Vendors/Vendors.jsx
@@ -16,6 +16,8 @@ class Vendors extends React.Component {
       this.state = {
          vendors: []
       };
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.componentDidMount = this.componentDidMount.bind(this);
    }
 
    componentDidMount() {
@@ -26,12 +28,37 @@ class Vendors extends React.Component {
       })
    }
 
+   handleInputChange(e) {
+      var searchTerm = e.target.value;
+      if(searchTerm == ""){
+         fetch('/vendors')
+         .then(res => res.json())
+         .then(vendors => {
+            this.setState({ vendors })
+         })
+      }
+      else{
+         fetch('/vendor/name/'+searchTerm, {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            }
+         })
+         .then(res => res.json())
+         .then(vendors => {
+            if(vendors != null){
+               this.setState({ vendors })
+            }
+         })
+      }
+    }
+
    render() {
       return(
          <div className={ styles.outerContainer }>
             <div className={ styles.searchBar }>
                <InputGroup>
-                  <Form.Control size="lg" placeholder="What are you looking for?" />
+                  <Form.Control controlId="searchTerm" size="lg" placeholder="What are you looking for?" onChange={e => this.handleInputChange(e)}/>
                   <DropdownButton as={InputGroup.Append} variant="outline-secondary" alignRight>
                      <DropdownItem>Search All</DropdownItem>
                      <Dropdown.Divider />
