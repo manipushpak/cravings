e7fbd64b27d99ee2fd6d6f2889d8e4cb2b5535f1
Sophia Jay Tiutan <stiutan@Sophia-Tiutan.local>
2019-03-01 14:17:23 -0800

    blah

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -322,14 +322,14 @@ const AddressSet = props => {
          <Form.Group>
             <Form.Label>Address</Form.Label>
             <InputGroup>
-               <Form.Control placeholder="Enter street address" id="vendorRegistrationLocation" className={ styles.inputStreetAddress } value={props.value} onChange={props.onChange} />
+               <Form.Control placeholder="Enter street address" id="vendorRegistrationLocation" className={ styles.inputStreetAddress } value={props.value} onChange={props.onChange} required />
                <InputGroup.Append className={ styles.inputGroupAppend }>
                   <Button variant="light" onClick={props.onClick}>Use Current Location</Button>
                </InputGroup.Append>
+               <Form.Control.Feedback type="invalid">Please enter your street address.</Form.Control.Feedback>
             </InputGroup>
             <Button size="sm" variant="light" className={ styles.inputGroupButton } onClick={props.onClick} block>Use Current Location</Button>
             <Form.Text id="userLocationText" className="text-muted"></Form.Text>
-            <Form.Control.Feedback type="invalid">Please enter your street address.</Form.Control.Feedback>
          </Form.Group>
       );
    }
