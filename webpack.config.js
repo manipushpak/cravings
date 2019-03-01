9ca8204dc57c43305e0235591e023b9222dbfc88
root <root@LAPTOP-QLA3B0J5.localdomain>
2019-03-01 15:28:33 -0800

    vendors page changes

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,15 @@ module.exports = {
         ],
       },
       {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      {
         test: /\.jsx?/,
         include: SRC_DIR,
         loader: 'babel-loader',
