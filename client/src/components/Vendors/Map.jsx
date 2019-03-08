1a2ba158b6b70e19055a30ad3207a193d13692ca
Sophia Jay Tiutan <stiutan@Sophia-Tiutan.local>
2019-03-07 17:07:43 -0800

    fixing connection of modal to map

--- client/src/components/Vendors/Map.jsx
+++ client/src/components/Vendors/Map.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import styles from '../../styles/Vendors/Map.css'
+import styles from '../../styles/Vendors/Map.css';
 
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+
+
+const GoogleMapElement = withGoogleMap(props => (
+   <GoogleMap
+      defaultCenter = { props.userLocation }
+      defaultZoom = { 13 }
+   >
+   {
+      props.vendors.map(vendor => {
+         return (
+            <Marker
+               key={ vendor.name }
+               position={ vendor.location.coordinates }
+               onClick={ () => props.setActiveKey(vendor.name) }
+            > 
+            { 
+               props.activeKey === vendor.name &&
+               <InfoWindow 
+                  onCloseClick={ () => props.setActiveKey(null) }
+               >
+                  <div onClick={ () => props.openModal(vendor.name, vendor.location.address, "9:00 AM - 5:00 PM") }>
+                     <p>{ vendor.name }</p>    
+                  </div>
+               </InfoWindow>
+            }
+            </Marker>
+         );
+      })
+   }
+   </GoogleMap>
+));
 
 class Map extends React.Component {
    constructor(props) {
       super(props);
       this.state = { 
          userLocation: { lat: 34.0224 , lng: -118.2851 }, 
-         loading: true
+         loading: true,
+         infoWindowVisible: false,
+         activeKey: null
       }
+
+      this.componentDidMount = this.componentDidMount.bind(this);
+      this.setActiveKey = this.setActiveKey.bind(this);
    }
 
    componentDidMount(props) {
@@ -28,34 +64,29 @@ class Map extends React.Component {
       );
    }
 
+   setActiveKey(key) {
+      this.setState({
+          activeKey: key,
+      });
+   }
+
    render() {
-      const { userLocation, loading } = this.state;
-      
+      const {  loading } = this.state;
+
       if (loading) {
          return <div className={ styles.loadingDiv }>Loading...</div> ;
       }
-
-      const GoogleMapElement = withGoogleMap(props => (
-         <GoogleMap
-            defaultCenter = { userLocation }
-            defaultZoom = { 13 }
-         >
-            {
-               this.props.vendors.map(vendor => {
-                  return (<Marker
-                     key={ vendor.name }
-                     position={ vendor.location.coordinates }
-                  ></Marker>);
-               })
-            }            
-         </GoogleMap>
-      ));
 
       return(
          <div>
             <GoogleMapElement
                containerElement={ <div style={{ height: '500px', width: '100%' }} /> }
                mapElement={ <div style={{ height: '100%' }} /> }
+               activeKey={ this.state.activeKey }
+               openModal={ this.props.openModal }
+               setActiveKey={ this.setActiveKey }
+               userLocation={ this.state.userLocation }
+               vendors={ this.props.vendors }
             />
          </div>
       );
