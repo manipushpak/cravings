6a5cb055ab607e26f4d5c1c8e54926b8a15a8023
Sophia Jay Tiutan <stiutan@Sophia-Tiutan.local>
2019-03-14 18:30:58 -0700

    small changes

--- client/src/components/Vendors/ListItem.jsx
+++ client/src/components/Vendors/ListItem.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import styles from '../../styles/Vendors/ListItem.css';
 
 const ListItem = props => {
-   var vendor = props.vendor;
-
    return(
       <div className={ styles.outerContainer } onClick = { props.openModal }>
          <div className={styles.greyBox}></div>
          <br />
-         <h3 className ={styles.h3}>{props.name}</h3>
+         <h3 className ={styles.h3}>{props.vendor.name}</h3>
       </div>
    );
 }
