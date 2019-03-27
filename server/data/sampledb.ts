2af066fdab93bd51c99126eb157ac78afa80f7a8
Devika Kumar <devikaku@usc.edu>
2019-03-26 18:52:40 -0700

    added more vendors

--- server/data/sampledb.ts
+++ server/data/sampledb.ts
@@ -69,8 +69,7 @@ const vendors: Vendor[] = [
             ]
         },
         
-}
-,
+},
 {
     loginInfo: {
         email:"sonalipa@usc.edu",
@@ -138,6 +137,134 @@ const vendors: Vendor[] = [
         ]
     },
     
+},
+{
+    loginInfo: {
+        email:"pia@usc.edu",
+        password:"hellohello2"
+    },
+    vendorInfo: {
+        vendorName: [{
+            firstName:"Pia",
+            lastName:"Tiutan"
+        }
+        ],
+        stallName: "Pia's Pudding",
+        phone: "6508239461",
+        address: {
+            address: "343 West Adams",
+            coordinates: {
+                lat: 555,
+                lng: 340
+            }
+        },
+        keywords: [
+            "pudding", "usc", "trojans"
+        ],
+        flags: ["v", "g-f", "h", "k"],
+        hours: [
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:false,
+                startTime: 1000,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:false,
+                startTime: 900,
+                endTime: 500,
+            }
+        ]
+    }
+    
+    
+},
+{
+    loginInfo: {
+        email:"miller@usc.edu",
+        password:"hellohello4"
+    },
+    vendorInfo: {
+        vendorName: [{
+            firstName:"Jeffrey",
+            lastName:"Miller"
+        }
+        ],
+        stallName: "Nalani's Girls Scout Cookies",
+        phone: "6508239461",
+        address: {
+            address: "Stauffer Science Lecture Hall",
+            coordinates: {
+                lat: 600,
+                lng: 340
+            }
+        },
+        keywords: [
+            "cookies"
+        ],
+        flags: ["v", "g-f", "h", "k"],
+        hours: [
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:false,
+                startTime: 1000,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:true,
+                startTime: 900,
+                endTime: 500,
+            },
+            {
+                open:false,
+                startTime: 900,
+                endTime: 500,
+            }
+        ]
+    }
 }
 ]
 
